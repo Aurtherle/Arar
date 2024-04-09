2d23d56683e2a8985990d1ce104cb20e7be3943c
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2024-04-09 06:07:55 +0300

    Update ميم2.js

--- plugins/ميم2.js
+++ plugins/ميم2.js
@@ -1,12 +1,11 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* زورو معانا+زورو ضايع`
+  let too = `[❗] افصل النص بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* زورو معانا+زورو ضايع`
 
   if (!text) throw too
   if (!text.includes('+')) throw too  
   let [a, b] = text.split`+`   
   let lr = (`https://api.popcat.xyz/pooh?text1=${a}&text2=${b}`)
-  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
-  𝑍𝑂𝑅𝑂⚡3𝑀𝐾`, m)
+  conn.sendFile(m.chat, lr, 'drake.png', `تست`, m)
 }
 handler.help = ['drake']
 handler.tags = ['maker']
