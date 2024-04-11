e30c717f5861c07c14974e04ac57a2c4c8be4fc6
aurtherle <hatg4179@gmail.com>
2024-04-11 22:32:45 +0300

    update

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
   if (!text && !(m.quoted && m.quoted.text)) {
-    throw `*هـذا هـو chatgpt اكـتب سـؤالـك وسـيرد عـليك*\nمـثال:\n*.زورو اريـد بعـض النـصائح لـعيش حيـاه سـعيده*\n\n*.زورو كـيف ابـدأ فـي مـجال الـبرمجه*`;
+    throw `*كيف يمكنني مساعدتك ؟*`;
   }
 
   if (!text && m.quoted && m.quoted.text) {
@@ -12,8 +12,8 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
   try {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/abafb0983183373f95cf4.png' },
-      caption: 'ثانيه افكر....⚡'
+      image: { url: 'https://telegra.ph/file/aa7d10713eb6289db2f1e.jpg' },
+      caption: 'هممم...'
     }, { quoted: m });
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
@@ -26,7 +26,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     let response = await fetch(guru2);
     let data = await response.json();
     let result = data.completion;
-    let yourName = '𝐵𝑌:𝑍𝑂𝑅𝑂⚡𝐵𝑂𝑇'; // قم بتعيين اسمك هنا
+    let yourName = 'ارثر ف الخدمة'; // قم بتعيين اسمك هنا
 
     await conn.relayMessage(m.chat, {
       protocolMessage: {
@@ -46,6 +46,6 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 };
 handler.help = ['chats'];
 handler.tags = ['ذكاء صناعي'];
-handler.command = ['زورو', 'زوز'];
+handler.command = ['ارثر', 'ارث'];
 
 export default handler;
