a68cd570b2bf7c8ace6f95b41c90d6cb03c825d8
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2024-04-11 21:42:50 +0300

    Update downloader-igstory.js

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -1,21 +1,21 @@
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-  if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] يرجى إرسال اسم مستخدم Instagram مثال: ${usedPrefix + command} yosef_zoro_3mk*`;
+  if (!args[0]) throw `*مثال : .ستوري billieeilish*`;
   await m.reply(global.wait);
   const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`);
   const anu = await res.json();
   const anuku = anu.result;
-  if (anuku == '') return m.reply('*[❗] لا توجد قصص حاليًا*');
+  if (anuku == '') return m.reply('*ليس لديه ستوري حاليا*');
   for (const i of anuku) {
     const res = await axios.head(i);
     const mime = res.headers['content-type'];
     if (/image/.test(mime)) {
       await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => {
-        return m.reply('*[❗] فشل في تحميل الصورة من الإنستجرام*');
+        return m.reply('*حدث خطأ*');
       });
     }
     if (/video/.test(mime)) {
       await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => {
-        return m.reply('*[❗] فشل في تحميل الفيديو من الإنستجرام*');
+        return m.reply('*حدث خطأ*');
       });
     }
   }
